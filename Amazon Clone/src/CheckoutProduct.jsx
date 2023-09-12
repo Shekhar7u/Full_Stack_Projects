7c5b531395e221ed8d3b1d34e6db1b8d79b62933
shekhar <singhskr7@gmail.com>
2023-09-12 22:37:51 +0530

    product checkout functionality added

---
+++ Amazon Clone/src/CheckoutProduct.jsx
@@ -0,0 +1,37 @@
+import { useStateValue } from "./StateProvider";
+import "./css/CheckoutProduct.css";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ cart }, dispatch] = useStateValue();
+
+  const removeFromCart = () => {
+    //remove item from cart
+    dispatch({
+        type:'Remove_From_Cart',
+        id: id,
+    })
+  };
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct_image" src="{image}" alt="" />
+
+      <div className="checkoutProduct_info">
+        <p className="checkoutProduct_title">{title}</p>
+        <p className="checkoutProduct_price">
+          <small>₹</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct_rating">
+          {Array(rating)
+            .fill()
+            .map(() => (
+              <p>🌟</p>
+            ))}
+        </div>
+        <button onClick={removeFromCart}>Remove from Cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
