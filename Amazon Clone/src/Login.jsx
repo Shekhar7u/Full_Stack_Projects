60162dc5b73a8791d5b8a4b209f95867e82d69af
shekhar <singhskr7@gmail.com>
2023-09-13 23:45:54 +0530

    user authentication with firebase

--- Amazon Clone/src/Login.jsx
+++ Amazon Clone/src/Login.jsx
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link,useHistory } from "react-router-dom";
 import { useState } from "react";
 import "./css/Login.css";
+import { auth } from "./fireBase";
 
 function Login() {
+  const history=useHistory()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+    .signInWithEmailAndPassword(email, password)
+    .then(auth => {
+      history.push('/')
+    })
+    .catch(err => alert(err.message))
   };
 
   const register = (e) => {
     e.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((auth) => {
+      //Successfully created user with email and password
+      console.log(auth)
+      if(auth){
+        history.push('/')
+      }
+    })
+    .catch((err) => alert(err.message))
   };
   return (
     <div className="login">
---
+++ Amazon Clone/src/fireBase.jsx
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyDSLDLC8AZ7acGGqcS-Vyvz41bm4FP5_zM",
+    authDomain: "clone-e95b5.firebaseapp.com",
+    projectId: "clone-e95b5",
+    storageBucket: "clone-e95b5.appspot.com",
+    messagingSenderId: "500401010661",
+    appId: "1:500401010661:web:92e9326ba03f482c35610c",
+    measurementId: "G-YP1L83NK78"
+  };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
+
+
+  const db=firebaseApp.firestore()
+  const auth=firebase.auth()
+
+  export {db, auth}
